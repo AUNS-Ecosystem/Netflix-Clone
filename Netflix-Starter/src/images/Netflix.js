cac6c2af1d34af9556fb7745c8e13142b6546ba0
AUNS Ecosystem <102518589+AUNS-Ecosystem@users.noreply.github.com>
2022-04-11 17:40:10 +0200

    Add files via upload

---
+++ Netflix-Starter/src/images/Netflix.js
@@ -0,0 +1,6 @@
+export const Logo = () => (
+<div>
+<img src="https://img1.wsimg.com/isteam/ip/e3f17df6-db9a-4fa4-b6e1-f7337e6674cd/AUNS%20LOGO%20FIN-0001.png/:/rs=w:91,h:94,cg:true,m/cr=w:91,h:94/qt=q:95" width="50px" height="50px">
+</img>
+</div>
+)
