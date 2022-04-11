a6aa2c68f4f88b404f7afac92a71707edae07934
AUNS Ecosystem <102518589+AUNS-Ecosystem@users.noreply.github.com>
2022-04-11 17:40:39 +0200

    Add files via upload

---
+++ Netflix-Starter/src/helpers/library.js
@@ -0,0 +1,138 @@
+import introS from "../images/introscene.png";
+import int from "../images/intro.jpg";
+import char from "../images/3dchar.PNG";
+import death from "../images/DeathMatch_Moment.jpg";
+
+import popeye1 from "../images/Popeye1.PNG";
+import popeye2 from "../images/popeye2.PNG";
+import popeye3 from "../images/popeye3.PNG";
+import cc1 from "../images/ccnjpt1.PNG";
+import cc2 from "../images/ccnjpt2.PNG";
+import cc3 from "../images/ccnewjobpt3.PNG";
+import creator from "../images/Studio-Project.png";
+
+export const movies = [
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmXCRLPiDYCoRAJmN96UoT3itDG4A9scsGM73bj26DC5GY/media/0",
+    Thumnbnail: int,
+    Scene: introS,
+    Name: "AUNS Demo Game Intro",
+    Description: "If using this code for your project,Please replace Logos and other content with your own original content.",
+    Genre: "Gaming",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 2min",
+},
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmesTw8ChdhG25NHMrPJfVvhUzsFRU7iieNkMNZM2fBkaU/media/0",
+    Thumnbnail: char,
+    Scene: char,
+ 
+    Name: "First AUNS 3D Character",
+    Description: "First AUNS 3D character. Shooting game with weapon menu updates.",
+    Genre: "Gaming",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 3min",
+},
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmZbD7pFivLRcEpwcndN7Drxkr9e1AMy2MArbfhsjqzfPA/media/0",
+    Thumnbnail: death,
+    Scene: death,
+  
+    Name: "AUNS Death Meatch",
+    Description: "Death Match Demo Game built with Core Games.",
+    Genre: "Gaming",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 2min",
+},
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmZxaUjUc1ekmEfeYbuQjG2uE4kzhLzs91nWpTydQ6oUk9/media/0",
+    Thumnbnail: popeye1,
+    Scene: popeye1,
+  
+    Name: "PopeyePart1",
+    Description: "Popeye Meets Sinbad Cartoon part 1. (Copyright free - Public Domain)",
+    Genre: "Cartoon",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 5min",
+},
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmPv9vFNzPMzNDwCbKFnhZYgEQp9Gbwt3WxiTfhKWfmbNW/media/0",
+    Thumnbnail: popeye2,
+    Scene: popeye2,
+  
+    Name: "PopeyePart2",
+    Description: "Popeye Meets Sinbad Cartoon part 2. (Copyright free - Public Domain)",
+    Genre: "Cartoon",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 5min",
+},
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmaGSmyAiVjkv2JQecf8nzikmTnd3H3PzUaXVKG39CR5DX/media/0",
+    Thumnbnail: popeye3,
+    Scene: popeye3,
+  
+    Name: "PopeyePart3",
+    Description: "Popeye Meets Sinbad Cartoon part 3. (Copyright free - Public Domain)",
+    Genre: "Cartoon",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 5min",
+},
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmT3BYAWKehEGgHwXDfTYigBnDuYPdK5UqRKzxUUCsGDjs/media/0",
+    Thumnbnail: cc1,
+    Scene: cc1,
+  
+    Name: "CC New Job",
+    Description: "CC gets a new job - Silent Movie Part 1. (Copyright free - Public Domain)",
+    Genre: "Classic",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 5min",
+},
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmXgpLk33cLYTQSvaPNx6LMPAX7VK2YiPxRUdshC14jpJb/media/0",
+    Thumnbnail: cc2,
+    Scene: cc2,
+  
+    Name: "CC New Job2",
+    Description: "CC gets a new job - Silent Movie Part 2. (Copyright free - Public Domain)",
+    Genre: "Classic",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 5min",
+},
+
+{
+    Movie: "https://ipfs.moralis.io:2053/ipfs/QmWbdda9eHcne16Vy3sLNxCMxteU1yVnSh6W6R1WjDWHtR/media/0",
+    Thumnbnail: cc3,
+    Scene: cc3,
+  
+    Name: "CC New Job3",
+    Description: "CC gets a new job - Silent Movie Part 3. (Copyright free - Public Domain)",
+    Genre: "Classic",
+    Actors: "n/a",
+    Year: "2022",
+    Duration: "Duration: 5min",
+},
+
+{
+    
+    Scene: creator,
+  
+   
+},
+
+
+];
